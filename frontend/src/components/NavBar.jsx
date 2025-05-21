4a177fd642fdb893674c7f465dd928abe1d972db
Ashwin <ashwincusat123@gmail.com>
2025-05-21 22:56:35 +0530

    Initial commit

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import { Link, useLocation } from 'react-router-dom'
+import '../css/Navbar.css'
+
+function NavBar() {
+  const location = useLocation();
+  
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        MovieFinder
+      </div>
+      <div className="navbar-links">
+        <Link to="/" className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}>
+          Home
+        </Link>
+        <Link to="/favorites" className={`nav-link ${location.pathname === '/favorites' ? 'active' : ''}`}>
+          Favorites
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
